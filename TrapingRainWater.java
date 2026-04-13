0821f773cda998c5426393f7e004031482309dff
sarasupadhyay <sarasupa093@gmail.com>
2026-04-13 17:54:23 +0530

    updated code

---
+++ TrapingRainWater.java
@@ -0,0 +1,67 @@
+package DSA;
+
+public class TrapingRainWater {
+    public int trap(int[] height) 
+    {
+        int n=height.length;
+    int leftmax[]=new int[n];
+    int rightmax[]=new int[n];
+     
+     leftmax[0]=height[0];
+     rightmax[n-1]=height[n-1];
+
+     for(int i=1;i<n;i++)
+     {
+        leftmax[i]=Math.max(leftmax[i-1], height[i]);
+     }
+      for(int i=n-2;i>=0;i--)
+     {
+        rightmax[i]=Math.max(rightmax[i+1], height[i]);
+     }
+
+     int water=0;
+      for(int i=0;i<n;i++)
+     {
+        water+=Math.min(leftmax[i], rightmax[i])-height[i];
+     }
+     return water;
+        
+    }
+     public int trap1(int[] height){
+        int n=height.length;
+        int l=0;
+        int r=n-1;
+        int lmax=0; int rmax=0;
+        int total=0;
+        while(l<r)
+        {
+            if(height[l]<=height[r])
+            {
+                if(lmax>=height[l])
+                {
+                    total+=lmax-height[l];
+                }
+                else{
+                    lmax=height[l];
+                }
+                l++;
+            }
+            else
+            {
+                if(rmax>=height[r])
+                {
+                    total+=rmax-height[r];
+                }
+                else
+                {
+                    rmax=height[r];
+                }
+                r--;
+            }
+        }
+        return total;
+    }
+    
+    
+}   
+
